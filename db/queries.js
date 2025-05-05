e67fdda82be841072a64cc0938a73aaeb6cbe67c
gabs <gabs712.g@gmail.com>
2025-05-05 16:40:26 -0300

    Make getPokemons also get by id

--- db/queries.js
+++ db/queries.js
@@ -5,14 +5,15 @@ async function getTypes() {
   return rows
 }
 
-async function getPokemons(type = null) {
+async function getPokemons(type = null, id = null) {
   const { rows } = await pool.query(
     `SELECT pokemons.id, pokemons.name, types.name AS type, hp, attack, notes FROM pokemons
       JOIN pokemon_types ON pokemons.id = pokemon_types.pokemon
       JOIN types ON types.name = pokemon_types.type
-      WHERE $1::text IS NULL OR types.name = $1
+      WHERE ($1::text IS NULL OR types.name = $1)
+      AND ($2::int IS NULL OR pokemons.id = $2)
     `,
-    [type],
+    [type, id],
   )
 
   return rows
