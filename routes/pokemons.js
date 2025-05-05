547f99ba91cdac67afac9bb15d7347480a8ecf9c
gabs <gabs712.g@gmail.com>
2025-05-05 16:49:55 -0300

    Handle edit route

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
-const { getTypes, getPokemons } = require('../db/queries')
+const { getPokemons } = require('../db/queries')
 
 const route = Router()
 
 route.get('/:id/edit', async (req, res) => {
   const pokemonId = req.params.id
+  const pokemon = (await getPokemons(null, pokemonId))[0]
 
-  res.send('test ' + pokemonId)
+  res.render('edit', { pokemon: pokemon })
 })
 
 module.exports = route
