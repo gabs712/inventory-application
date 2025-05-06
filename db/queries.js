d8111cead074faee36007441f11013f3dae0fce7
gabs <gabs712.g@gmail.com>
2025-05-06 11:38:21 -0300

    Add insertPokemon function

--- db/queries.js
+++ db/queries.js
@@ -20,7 +20,39 @@ async function getPokemons(type = null, id = null) {
   return rows
 }
 
+async function hasType(type) {
+  const { rows } = await pool.query('SELECT * FROM types WHERE name = $1', [
+    type,
+  ])
+
+  return rows.length >= 1
+}
+
+async function insertPokemon(name, type, hp, attack, notes) {
+  if (!(await hasType(type))) {
+    await pool.query(`INSERT INTO types VALUES($1)`, [type])
+  }
+
+  const { rows } = await pool.query(
+    `
+      INSERT INTO pokemons (name, hp, attack, notes) VALUES ($1, $2, $3, $4) RETURNING id
+    `,
+    [name, hp, attack, notes],
+  )
+
+  const id = rows[0].id
+
+  await pool.query(
+    `
+      INSERT INTO pokemon_types VALUES ($1, $2)
+    `,
+    [id, type],
+  )
+}
+
 module.exports = {
   getTypes,
   getPokemons,
+  insertPokemon,
+  hasType,
 }
